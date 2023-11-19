537be173b3e43be52d9e76bdeb12dbfd9ad94cfd
やしん <60528781+yaxcholi@users.noreply.github.com>
2023-11-19 14:30:33 +0300

    Update vowelcheck.java

--- vowelcheck.java
+++ vowelcheck.java
@@ -1,37 +1,35 @@
-  import java.util.Scanner;
+import java.util.Scanner;
 
-  public class vowelcheck {
+public class vowelcheck {
 
-      public static boolean isVowel(char ch) {
-          char uppercaseChar = Character.toUpperCase(ch);
-          return uppercaseChar == 'A' || uppercaseChar == 'E' || uppercaseChar == 'I' || uppercaseChar == 'O' || uppercaseChar == 'U';
-      }
+    public static boolean isVowel(char ch) {
+        char uppercaseChar = Character.toUpperCase(ch);
+        return uppercaseChar == 'A' || uppercaseChar == 'E' || uppercaseChar == 'I' || uppercaseChar == 'O' || uppercaseChar == 'U';
+    }
 
-      public static void countVowels() {
-          Scanner scanner = new Scanner(System.in);
-          int vowelCount = 0;
+    public static void countVowels() {
+        Scanner scanner = new Scanner(System.in);
+        int vowelCount = 0;
 
-          System.out.println("Enter characters (. to quit): ");
+        System.out.println("Enter characters (. to quit): ");
 
-          char inputChar = ' ';
+        char inputChar = ' ';
 
-          while (inputChar != '.') {
-              System.out.print("Enter a character (or '.' to quit): ");
-              String userInput = scanner.next();
+        while (inputChar != '.') {
+            System.out.print("Enter a character (or '.' to quit): ");
+            String userInput = scanner.next();
 
-              if (!userInput.isEmpty()) {
-                  inputChar = userInput.charAt(0);
+            inputChar = userInput.charAt(0);
 
-                  if (isVowel(inputChar)) {
-                      vowelCount++;
-                  }
-              }
-          }
+            if (isVowel(inputChar)) {
+                vowelCount++;
+            }
+        }
 
-          System.out.println("Found " + vowelCount + " vowels.");
-      }
+        System.out.println("Found " + vowelCount + " vowels.");
+    }
 
-      public static void main(String[] args) {
-          countVowels();
-      }
-  }
+    public static void main(String[] args) {
+        countVowels();
+    }
+}
